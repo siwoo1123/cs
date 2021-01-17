9996ba9b3db7fe28edd35339788bc1c92502cb67
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-01-17 18:48:09 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,17 @@ int main()
     
     scanf("%lf %lf %lf", &ipt[0], &ipt[1], &ipt[2]);
     
+    for (int i = 0; i < 3 - 1; i++) {
+        for (int j = i + 1; j < 3; j++) {
+            if (ipt[i] < ipt[j]) {
+                double tmp = ipt[i];
+                ipt[i] = ipt[j];
+                ipt[j] = tmp;
+            }
+        }
+    }
     
+    printf("%.0f %.0f %.0f \n", ceil(ipt[0]), floor(ipt[2]), round(ipt[1]));
     
     return 0;
 }
