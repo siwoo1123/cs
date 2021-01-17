4b66a0ca14bfd8103651b48fc160a260666c2ac6
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-01-17 17:26:05 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,15 @@
 #include <stdio.h>
-
-int day[13] = {
-  0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
-};
-
-void okbad()
-{
-  int a, b;
-  scanf("%d %d", &a, &b);
-
-  if(a <= 0 || a >= 13) {
-    printf("BAD!\n");
-    return;
-  }
-  if (day[a] < b || b <= 0) {
-    printf("BAD!\n");
-    return;
-  }
-  printf("OK!\n");
-}
+#include <math.h>
 
 int main()
 {
-  okbad();
-  return 0;
+    double area;
+    double base;
+    double exp;
+    
+    printf("정사각형의 넓이 : ");
+    scanf("%lf", &area);
+    printf("정사각형의 한 변의 길이 : %f \n", sqrt(area));
+    
+    return 0;
 }
