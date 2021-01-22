7d4a74d20adc67c18e067c8a842bc3c03227690e
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-01-22 21:01:49 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-
-void todo(int a, int b, int c) {
-    printf("%.0f %.0f %.0f\n", ceil(a), floor(c), round(b));
-}
-
+ 
 int main()
 {
-    double ipt[3];
-    
-    scanf("%lf %lf %lf", &ipt[0], &ipt[1], &ipt[2]);
-    
-    for (int i = 0; i < 3 - 1; i++) {
-        for (int j = i + 1; j < 3; j++) {
-            if (ipt[i] < ipt[j]) {
-                double tmp = ipt[i];
-                ipt[i] = ipt[j];
-                ipt[j] = tmp;
-            }
-        }
-    }
-    
-    printf("%.0f %.0f %.0f \n", ceil(ipt[0]), floor(ipt[2]), round(ipt[1]));
-    
+    int ipt;
+     
+    scanf("%d", &ipt);
+     
+    printf("%.0f\n", pow(2, ipt));
+     
     return 0;
 }
