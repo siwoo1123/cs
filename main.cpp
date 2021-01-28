03ff23125ef87720984b2420e83ee9beb6927eb8
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-01-28 16:53:20 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 #include <stdio.h>
-#include <math.h>
- 
+
+int arr[102] = {
+    0, 1, 2
+};
+
+void newarr(int n)
+{
+    if (n > 100) return;
+    arr[n] = (arr[n - 1] * arr[n - 2]) % 100;
+    newarr(n + 1);
+}
+
 int main()
 {
-    int ipt;
-     
-    scanf("%d", &ipt);
-     
-    printf("%.0f\n", pow(2, ipt));
-     
+    int N;
+    scanf("%d", &N);
+    newarr(3);
+    printf("%d\n", arr[N]);
     return 0;
 }
