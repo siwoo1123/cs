e107504c6e070d45e07788060e611c976e5935c7
siwoo1123 <61868624+siwoo1123@users.noreply.github.com>
2021-01-30 14:50:44 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,18 @@
 #include <stdio.h>
-
-int a, b, c; // a,b,c선언
-
-void input()
-{
-    scanf("%d %d %d", &a, &b, &c); // 입력
-    a *= b * c; // 계산값을 a에 저장
-}
-
-int gasan(int a)
-{
-    if(a == 0) return 1; // a가 0이면 1을 리턴
-    int r = a % 10; //  int변수 r에 a를 10으로 나눈 나머지 대입
-    if (r == 0) { // r이 0이면
-        r = 1; // r에 1대입
-    }
-    return gasan(a / 10) * r; // gasan(a/10)에 r을 곱한값 리턴
-}
+#include <string.h>
 
 int main()
 {
-    input();
-    int result = gasan(a);
-    printf("%d\n", result);
+    int i, len, realLen;
+    char str[100];
+    
+    scanf("%s %d", str, &len);
+    
+    realLen = strlen(str);
+    
+    for (i = realLen; i >= len; i--) {
+        printf("%c", str[i - 1]);
+    }
+    printf("\n");
     return 0;
 }
